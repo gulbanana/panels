eabe42f4985d11bf1aae08e6158e338aeb803343
Thomas Castiglione <castiglione@mac.com>
2016-01-17 17:25:45 +0800

    rows and columns

--- Box.tsx
+++ Box.tsx
@@ -1,8 +1,25 @@
 ﻿import React = require('react');
 import ReactDOM = require('react-dom');
 
-export default class Box extends React.Component<any, any> {
+interface Props
+{
+    label: string;
+    width?: number;
+    height?: number;
+}
+
+export default class Box extends React.Component<Props, any> {
+    static defaultProps: Props = {
+        label: "",
+        width: 100,
+        height: 100  
+    }
+    
 	render() {
-        return <div style={{width: 50, height: 50, background: 'blue', border: '2px solid red'}} />;
+        return <div style={{width: this.props.width, height: this.props.height, boxSizing: 'border-box', border: '1px solid white'}}>
+            <div style={{width: '100%', height: '100%', background: 'blue', boxSizing: 'border-box', border: '2px solid red'}}>
+                <p style={{color: 'white', textAlign: 'center', fontWeight: 'bold'}} >{this.props.label}</p>
+            </div>
+        </div>;
 	}
 }
